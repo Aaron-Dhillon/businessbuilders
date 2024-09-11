4f499293bdc58c04d8b97ae77a350725eadf4e87
prathkan03 <prathkan03@gmail.com>
2024-09-11 17:17:29 -0400

    headshots

--- src/pages/Leadership.js
+++ src/pages/Leadership.js
@@ -1,6 +1,12 @@
 import "../App.css";
 import styles from "../assets/styles/Home.module.css";
 import Logo from "../assets/Business Builders Logo.png";
+import Rohan from "../assets/rsuggala.png";
+import Trevor from "../assets/tgerald.png";
+import Kathir from "../assets/kmaari.png";
+import Henry from "../assets/henry.png";
+import Jen from "../assets/jhazlett.jpg";
+import Pratham from "../assets/pratham.kancherla_headshot.png";
 import Connect from "../assets/Connect.svg";
 import Educate from "../assets/Educate.svg";
 import Inspire from "../assets/Inspire.svg";
@@ -37,14 +43,17 @@ function Leadership() {
           <span className={styles.missionTitle}>Our Leadership</span>
           <div className={styles.missionItemsContainer}>
             <div className={styles.missionItemContainer}>
+              <img src= {Rohan}></img>
               <span className={styles.landingTitle}>Rohan Suggala<br/></span>
               <span className={styles.missionItemDescription}>President</span>
             </div>
             <div className={styles.missionItemContainer}>
+              <img src={Trevor}></img>
             <span className={styles.landingTitle}>Trevor Gerald<br/></span>
               <span className={styles.missionItemDescription}>Vice President of Operations</span>
             </div>
             <div className={styles.missionItemContainer}>
+              <img src={Pratham}></img>
             <span className={styles.landingTitle}>Pratham Kancherla<br/></span>
               <span className={styles.missionItemDescription}>Vice President of Technology</span>
             </div>
@@ -57,36 +66,19 @@ function Leadership() {
           <span className={styles.missionTitle}>Our Leadership</span>
           <div className={styles.missionItemsContainer}>
             <div className={styles.missionItemContainer}>
+              <img src = {Henry}></img>
               <span className={styles.landingTitle}>Henry Guerrera<br/></span>
               <span className={styles.missionItemDescription}>Vice President of Finance</span>
             </div>
             <div className={styles.missionItemContainer}>
-            <span className={styles.landingTitle}>Kathir Maarikarthykeyan<br/></span>
+              <img src = {Kathir}></img>
+            <span className={styles.landingTitle}>Kathir M.<br/></span>
               <span className={styles.missionItemDescription}>Vice President of Marketing</span>
             </div>
             <div className={styles.missionItemContainer}>
+              <img src={Jen}></img>
             <span className={styles.landingTitle}>Jen Hazlett<br/></span>
               <span className={styles.missionItemDescription}>Vice President of Socials</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.scrollSubcontainer}>
-        <div className={styles.missionSubContainer}>
-          <span className={styles.missionTitle}>Our Mission</span>
-          <div className={styles.missionItemsContainer}>
-            <div className={styles.missionItemContainer}>
-              <img className={styles.missionItemImage} src={Connect} alt="Business Builders - Connect Pillar"></img>
-              <span className={styles.missionItemDescription}>Build a community of innovators, problem-solvers, and doers with relationships that will last a lifetime.</span>
-            </div>
-            <div className={styles.missionItemContainer}>
-              <img className={styles.missionItemImage} src={Educate} alt="Business Builders - Educate Pillar"></img>
-              <span className={styles.missionItemDescription}>Provide members with the knowledge required to build a sustainable business, think creatively, and achieve career goals.</span>
-            </div>
-            <div className={styles.missionItemContainer}>
-              <img className={styles.missionItemImage} src={Inspire} alt="Business Builders - Inspire Pillar"></img>
-              <span className={styles.missionItemDescription}>Inspire students to explore and realize new career heights and life possibilities that are within their reach.</span>
             </div>
           </div>
         </div>
